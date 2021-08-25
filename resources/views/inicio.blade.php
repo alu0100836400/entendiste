0c281dae06e2537872468d695fe921ac291b5745
alu0100836400 <alu0100836400@ull.edu.es>
2021-08-25 19:46:37 +0100

    texto de inicio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.app')
 
 @section('title', 'Inicio')
+@section('head')
+    <link rel="stylesheet" href="{{asset('css/inicio.css')}}">
+@endsection
 
 @section('content')
-    Contenido inicial ?¿?¿
+<p>
+    <span>
+        Esta web forma parte del Trabajo de Fin de Grado <br><br><label class="italic">"Herramienta para la obtención de 
+        feedback por parte del alumnado".</label> <br><br>Escuela Superior de Ingeniería y Tecnología. 
+        <br>Universidad de La Laguna
+    </span>
+    &mdash; Javier Alberto Martín &mdash;
+  </p>
+
 @endsection
